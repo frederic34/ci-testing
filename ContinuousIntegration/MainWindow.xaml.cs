2ad6581979bc28d16717e4989d37e158b2aa6b8d
FRANCE Frédéric <frederic.france@free.fr>
2017-03-28 18:39:01 +0200

    test unit

--- ContinuousIntegration/MainWindow.xaml.cs
+++ ContinuousIntegration/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace ContinuousIntegration
 {
@@ -23,6 +10,16 @@ namespace ContinuousIntegration
 		public MainWindow()
 		{
 			InitializeComponent();
+			if (1==1)
+			{
+				if (2==2)
+				{
+					if (3==3)
+					{
+						System.Console.WriteLine("Hello World!");
+					}
+				}
+			}
 		}
 	}
 }
---
+++ UnitTestContinuousIntegration/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ContinuousIntegration;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestContinuousIntegration
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			int rendu = 2;
+			bool result = true;
+			Assert.IsTrue(result);
+			Assert.AreEqual(rendu, 2);
+		}
+	}
+}
